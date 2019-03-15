207d5654da37d64c0def162a1ea214e33939cb4d
Alexey Zirenko <42088023+Emiro512@users.noreply.github.com>
2019-03-15 10:22:51 +0200

    Update albums.js

--- js/albums.js
+++ js/albums.js
@@ -10,14 +10,6 @@
          ]
      },
      {
-        name:'Москва',
-        address:'Москва',
-        links: ['https://varlamov.me/2017/kak_menyaetsya_moskva/00s.jpg',
-                'https://www.votpusk.ru/country/ctimages/new/moscow.jpg',
-                'http://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2018-09/BED07951.jpg?itok=VeiViMJd',
-        ]
-     },
-     {
          name:'A-level',
          address:'Харьков площадь павловская 6',
          links:['https://a-level.pl.ua/wp-content/themes/alevel/images/landscape.png',
@@ -33,13 +25,6 @@
                'http://kp.crimea.ua/wp-content/uploads/2018/07/kiev-header.jpg'
         ]
     },
-     {
-         name:'Одесса',
-         address:'Одесса',
-         links:['http://odessa.net.ua/Media/images/news-slider/big/f235ef726a1bb627e17db50a0986fe7a.jpg',
-             'https://focus.ua/files/infocenter-odessa.com_1514280874_68820021.jpg',
-         ]
-     },
      {
          name:'Белгород',
          address:'Белгород',
